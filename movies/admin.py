7ab91ed95a9ba52c89c3e942512fff72562ac981
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-08-01 22:38:32 +0200

    Delete movies directory

--- movies/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Movie, WatchedMovie, WatchlistMovie
-
-admin.site.register(Movie)
-admin.site.register(WatchedMovie)
-admin.site.register(WatchlistMovie)
