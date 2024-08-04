fe1b9570a2b0e54238e7607136b2f9c11c5b6011
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-08-04 21:40:07 +0200

    Add files via upload

---
+++ movies/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import landing, home, signup, login_view, levels, watchlist, update_movie_status, recommend_movies
+
+urlpatterns = [
+    path('', landing, name='landing'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('levels/', levels, name='levels'),
+    path('watchlist/', watchlist, name='watchlist'),
+    path('recommendations/', recommend_movies, name='recommendations'),
+    path('home/', home, name='home'),
+    path('update_movie_status/', update_movie_status, name='update_movie_status'),
+]
