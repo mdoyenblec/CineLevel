64e39a7697388b0b1d6d709e887e9ab75b6e9ab4
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2024-08-05 11:07:36 +0200

    Update settings.py

--- cinelevel/settings.py
+++ cinelevel/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*73=6y6+7lu86g(be9%0b8g*(&+#vn7q@1=_i()eb64o5p3j%%"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
